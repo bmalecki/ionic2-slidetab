989385f667d00441b12d18132ba0a76480ac1aa6
bmalecki <bmalecki995@gmail.com>
2016-08-14 21:42:19 +0200

    dummy slider tab

--- app/pages/about/about.ts
+++ app/pages/about/about.ts
@@ -10,31 +10,5 @@ export class AboutPage {
   constructor(private navCtrl: NavController) {
     this.items = [];
 
-    let items = [
-      {title: 'Bartek'},
-      {title: 'Kamil'},
-      {title: 'Anna'},
-      {title: 'Zofia'},
-      {title: 'Ela'},
-      {title: 'Patryk'}
-    ];
-
-
-
-    let copyItems = (id) => {
-      return items.map((v:any) => {
-        return {
-          title: `${id}: ${v.title}`
-        };
-      });
-    };
-
-    this.items.push(
-      ...copyItems(1),
-      ...copyItems(2),
-      ...copyItems(3),
-      ...copyItems(4),
-      ...copyItems(5)
-      );
   }
 }
--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -1,8 +1,12 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import { Slides } from 'ionic-angular';
+
 import {HomePage} from '../home/home';
 import {AboutPage} from '../about/about';
 import {ContactPage} from '../contact/contact';
 import {EmptyPage} from '../empty/empty';
+
+
 
 @Component({
   templateUrl: 'pages/tabs/tabs.html'
@@ -14,12 +18,35 @@ export class TabsPage {
   private tab3Root: any;
   private tab4Root: any;
 
-  constructor() {
+  currState = '0';
+  @ViewChild('MainSlider') slider: Slides;  
+
+  onTabUpdate(event){
+    let newIndexAfterUpdate = event.activeIndex;
+    this.currState = String(newIndexAfterUpdate);
+  }
+
+  onSegmentUpdate(event){
+    this.currState = event.value;
+
+    console.log(this.currState);
+
+    console.log(this.slider);
+
+    this.slider.slideTo( parseInt( this.currState));
+
+  }
+
+  constructor() {   
+
+
     // this tells the tabs component which Pages
     // should be each tab's root Page
-    this.tab1Root = AboutPage;
+    /*this.tab1Root = AboutPage;
     this.tab2Root = HomePage;
     this.tab3Root = ContactPage;
-    this.tab4Root = HomePage;
+    this.tab4Root = HomePage;*/
+
+
   }
 }
