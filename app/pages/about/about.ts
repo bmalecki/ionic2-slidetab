d06f9a9529928220f85c985c3c2adb3c3e8d3b60
bmalecki <bmalecki995@gmail.com>
2016-08-14 20:54:56 +0200

    experiments

--- app/pages/about/about.ts
+++ app/pages/about/about.ts
@@ -5,6 +5,36 @@ import {NavController} from 'ionic-angular';
   templateUrl: 'pages/about/about.html'
 })
 export class AboutPage {
+  items: Array<Object>;
+
   constructor(private navCtrl: NavController) {
+    this.items = [];
+
+    let items = [
+      {title: 'Bartek'},
+      {title: 'Kamil'},
+      {title: 'Anna'},
+      {title: 'Zofia'},
+      {title: 'Ela'},
+      {title: 'Patryk'}
+    ];
+
+
+
+    let copyItems = (id) => {
+      return items.map((v:any) => {
+        return {
+          title: `${id}: ${v.title}`
+        };
+      });
+    };
+
+    this.items.push(
+      ...copyItems(1),
+      ...copyItems(2),
+      ...copyItems(3),
+      ...copyItems(4),
+      ...copyItems(5)
+      );
   }
 }
--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,17 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild, ElementRef} from '@angular/core';
 import {NavController} from 'ionic-angular';
 
 @Component({
   templateUrl: 'pages/home/home.html'
 })
 export class HomePage {
+  count = 2;
+
+  incrementButton(){
+    ++this.count;
+  }
+
   constructor(private navCtrl: NavController) {
-  
+ 
   }
 }
